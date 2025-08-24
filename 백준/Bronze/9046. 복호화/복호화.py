62e192422690a9497938f39427299e0ed4223b11
김한선 <68306239+hansun-hub@users.noreply.github.com>
2025-08-24 20:50:47 +0900

    [Bronze II] Title: 복호화, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.py
+++ 백준/Bronze/9046. 복호화/복호화.py
@@ -1,17 +1,15 @@
 n=int(input())
+for _ in range(n):
+    alpa = [0]*26
+    word = input()
+    for i in word:
+        if i.isalpha():
+            index = ord(i)-ord('a')
+            alpa[index]+=1
 
-for _ in range(n) :
-    counts = [0] * 26
-    blnk = input()
-
-    for ch in blnk:
-        if ch.isalpha():
-            counts[ord(ch)-ord('a')]+=1
-
-    max_val = max(counts)
-
-    if counts.count(max_val) > 1:
+    max_val = max(alpa)
+    if alpa.count(max_val) >1:
         print("?")
     else:
-        max_idx = counts.index(max_val)
-        print(chr(max_idx+ord('a')))
+        max_index = alpa.index(max_val)
+        print(chr(max_index+ord('a')))
