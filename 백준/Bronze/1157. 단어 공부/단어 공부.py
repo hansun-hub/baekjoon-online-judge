701c3368ce2e0b2ddf2d353e79b71538f324eb89
김한선 <68306239+hansun-hub@users.noreply.github.com>
2025-10-01 07:14:03 +0900

    [Bronze I] Title: 단어 공부, Time: 116 ms, Memory: 36244 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,12 @@
-n=input()
-alpa=[0]*26
-n=n.upper()
-for ch in n:
-    index=(ord(ch)-ord('A'))
-    alpa[index]+=1
-a=max(alpa)
-if alpa.count(a)>1:
+from collections import Counter
+
+word = input().upper()
+counter = Counter(word)
+
+max_count = max(counter.values())
+most_common = [ch for ch,cnt in counter.items() if cnt==max_count]
+
+if len(most_common) > 1:
     print("?")
 else:
-    max_index=alpa.index(a)
-    result=chr(max_index+ord('A'))
-    print(result)
+    print(most_common[0])
